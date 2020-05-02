eb8e5ed86fc5034683eb458ad6bf21b6eddd53c8
NormanLYJ <leeyuejun@gmail.com>
2020-05-02 15:13:33 +0800

    add exception handlin gwhen loading in windows

--- 2_table_load.py
+++ 2_table_load.py
@@ -4,4 +4,7 @@ engine = create_engine('mysql://root:Test123!@localhost/oscres_db')
 import pandas as pd
 data_frame = pd.read_csv('Miscellaneous/university.csv')
 # con expect type sqlalchemy.engine.Engine or sqlite3.Connection
-data_frame.to_sql(con=engine, name='Recommender_university_course', if_exists='replace')
+try:
+    data_frame.to_sql(con=engine, name='recommender_university_course', if_exists='replace')
+except: 
+    data_frame.to_sql(con=engine, name='Recommender_university_course', if_exists='replace')
